763112bdc37e5f52fe57689e7865d9cefc8214a9
Ali Rezaali <alirezaali.web.78@gmail.com>
2020-09-06 01:36:34 +0430

    Add project files.

---
+++ DesignPatterns/Momento/EditorState.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns.Momento
+{
+    public class EditorState
+    {
+        public string Content { get;}
+
+        public EditorState(string content)
+        {
+            Content = content;
+        }
+    }
+
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,30 @@
+﻿using DesignPatterns.Momento;
+using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static void MomentoSection()
+        {
+            var editor = new Editor();
+            var history = new History();
+            editor.Content = "a";
+            history.Push(editor.CreateState());
+
+            editor.Content = "b";
+            history.Push(editor.CreateState());
+
+            editor.Content = "c";
+
+            editor.Restore(history.Pop());
+            editor.Restore(history.Pop());
+
+            Console.WriteLine(editor.Content);
+        }
+    }
+}
