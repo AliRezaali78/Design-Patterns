21232333f6fe352d287e6773fc06a473452418c7
Ali Rezaali <alirezaali.web.78@gmail.com>
2020-09-08 16:10:30 +0430

    Add TemplateMethodPattern Example

---
+++ DesignPatterns/Template Method Pattern/Example/Window.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DesignPatterns.Template_Method_Pattern.Example
+{
+    public abstract class Window
+    {
+        public void Close()
+        {
+            OnClosing();
+
+            Console.WriteLine("Closed...");
+
+            OnClosed();
+        }
+
+        protected abstract void OnClosing();
+
+        protected abstract void OnClosed();
+    }
+
+    public class ChatWindow : Window
+    {
+        protected override void OnClosing()
+        {
+            Console.WriteLine("Before Closing ...");
+        }
+
+        protected override void OnClosed()
+        {
+            Console.WriteLine("After Closing");
+        }
+    }
+
+}
